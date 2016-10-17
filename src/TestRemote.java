87c788bf532032c31ab176005fc49668004ee422
Yau Kuen Tsang <danielyktsang@gmail.com>
2016-10-17 09:59:33 -0700

    aaaa

--- src/TestRemote.java
+++ src/TestRemote.java
@@ -2,6 +2,7 @@
 public class TestRemote {
 	public static void  main(String[] args) {
 		System.out.println("Yes it does!!!");
-		System.out.println("change");
+		//change
+		System.out.println("changecccccccccc");
 	}
 }
